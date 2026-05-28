21c95eeb5006e46bd220eb5c8c516b9e56508579
Tyler West <tyleximus@gmail.com>
2026-05-28 19:47:27 -0400

    Add blend sort order mapping

--- InnerSky.WebApi/InnerSkyDbContext.cs
+++ InnerSky.WebApi/InnerSkyDbContext.cs
@@ -33,6 +33,8 @@ public sealed class InnerSkyDbContext(DbContextOptions<InnerSkyDbContext> option
             entity.Property(x => x.Name).HasMaxLength(200).IsRequired();
             entity.Property(x => x.CreatedUtc).HasColumnType("datetime2(0)").IsRequired();
             entity.Property(x => x.MomentId).IsRequired();
+            entity.Property(x => x.SortOrder).IsRequired();
+            entity.HasIndex(x => new { x.MomentId, x.SortOrder });
             entity.HasMany(x => x.Components)
                 .WithOne(x => x.Profile)
                 .HasForeignKey(x => x.ProfileId)
