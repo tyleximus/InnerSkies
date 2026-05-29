4cb8f4d05c8900b98d1eea6245536d3d09cd2d74
Tyler West <tyleximus@gmail.com>
2026-05-29 17:51:14 -0400

    cleanup

--- InnerSky.Wasm/Program.cs
+++ InnerSky.Wasm/Program.cs
@@ -6,8 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7036/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://innerskyapi.tyleximus.com/") });
 
 builder.Services.AddScoped<InnerSky.Wasm.Services.EmotionStateService>();
 builder.Services.AddScoped<InnerSky.Wasm.Services.EmotionProfilesApiClient>();
